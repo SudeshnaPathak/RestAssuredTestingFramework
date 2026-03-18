6524c5d4c18a08b4ddd34e647a4924e9b80a438a
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-03-18 21:44:49 +0530

    Add Reports plugin

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,6 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = {"src/test/resources/functionalTests"},
         glue = {"stepDefinitions"},
         monochrome = true,
+        plugin = {"json:target/jsonReports/RestAssuredTests.json"},
         dryRun = false)
 public class TestRunner extends AbstractTestNGCucumberTests {
 
