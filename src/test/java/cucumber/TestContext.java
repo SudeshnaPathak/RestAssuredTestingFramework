8b5a48b3a163231d5451bb15ded524bf30ad85ec
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-03-22 21:29:58 +0530

    Update TestContext & BaseStep to share common apiService instance

--- src/test/java/cucumber/TestContext.java
+++ src/test/java/cucumber/TestContext.java
@@ -1,16 +1,22 @@
 package cucumber;
 
+import apiEngine.ApiService;
 import enums.Context;
 import lombok.Getter;
+import utils.ConfigReader;
 
 import static utils.ReadDataFromPropertiesFile.userId;
 
 @Getter
 public class TestContext {
     private final ScenarioContext scenarioContext;
+    private final ConfigReader configReader = ConfigReader.getInstance();
+    private final ApiService apiService;
+
 
     public TestContext() {
         this.scenarioContext = new ScenarioContext();
         scenarioContext.setContext(Context.USER_ID, userId);
+        this.apiService = new ApiService(configReader.getBaseUrl());
     }
 }
--- src/test/java/stepDefinitions/BaseStep.java
+++ src/test/java/stepDefinitions/BaseStep.java
@@ -11,11 +11,12 @@ public class BaseStep {
 
     private final ScenarioContext scenarioContext;
     private final ApiService apiService;
-    private final ConfigReader configReader = ConfigReader.getInstance();
+    private final ConfigReader configReader;
 
     public BaseStep(TestContext testContext) {
         this.scenarioContext = testContext.getScenarioContext();
-        this.apiService = new ApiService(configReader.getBaseUrl());
+        this.apiService = testContext.getApiService();
+        this.configReader = testContext.getConfigReader();
     }
 
 }
